971ef1b64c09736edc844fffed5195ef83d5af00
WindyCheng520 <WindyCheng@aliyun.com>
2016-10-10 21:13:38 +0800

    test

---
+++ MytestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MytestProject
+//
+//  Created by Windy on 16/10/10.
+//  Copyright © 2016年 Windy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    }
+
+
+
+
+}
+
